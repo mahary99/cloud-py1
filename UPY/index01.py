92c63aa682e922dd07f88501d5c1a1e0eba7693c
mahary99 <93893824+mahary99@users.noreply.github.com>
2021-11-12 15:34:49 +0400

    Add files via upload

---
+++ UPY/index01.py
@@ -0,0 +1,7 @@
+import math
+print(" Program to demonstrate the use of Pyton library-")
+num = float(input("Enter a numper:"))
+print(math.sqrt(num))
+print(math.ceil(num))
+print(math.floor(num))
+
